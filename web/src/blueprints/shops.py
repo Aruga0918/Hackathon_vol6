8a1d13ae4c55fadf2eacc1fa69e03091ead21c69
マタタビのねこ <matatabinoneko0721@gmail.com>
2021-07-21 17:41:13 +0900

    apiの全pathを作成

--- web/src/blueprints/shops.py
+++ web/src/blueprints/shops.py
@@ -7,6 +7,39 @@ shops = Blueprint("shops", __name__)
 logger = logging.getLogger("app")
 
 
+@shops.route("/shops/<int:shop_id>", methods=["GET"])
+def get_user_info(user_id):
+    """
+    店舗情報取得API
+
+    Parameters
+    ----------
+
+    Returns
+    -------
+    {
+        "id" : shop_id
+        "name" : shop名
+        "latitude" : 緯度
+        "longitude" : 経度
+        "description" : 説明
+        "address" : 住所
+        "menu" : メニューのリスト [
+            {
+                "menu_id" : menuのid
+                "name": メニューの名前
+                "price" : 価格
+            }
+            ...
+        ],
+    }
+    """
+    logger.warn("warn")
+    logger.error("error")
+    logger.critical("critical")
+    return jsonify({"message": "api_test"}), HTTPStatus.OK
+
+
 @shops.route("/shops/test")
 def index():
     logger.warn("warn")
