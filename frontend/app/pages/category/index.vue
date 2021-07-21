771d5f0aa0a4e8e2670ee9af2847073a2e25a6ac
有賀優太 <yuta@arigayuutanoAir.local>
2021-07-21 20:02:27 +0900

    カテゴリメニューをレスポンシブ対応

--- frontend/app/pages/category/index.vue
+++ frontend/app/pages/category/index.vue
@@ -1,11 +1,25 @@
 <template>
   <div class="container" style="text-align: center">
-    <div class="row" style="width: 90vh">
-      <div v-for="category in categories" :key="category" class="card col-6">
-        <img class="card-img" :src="category.IconUrl" alt="" />
-        <div class="card-img-overlay">
-          <p class="text-white">{{ category.CategoryName }}</p>
-        </div>
+    <div class="row" style="width: 100%">
+      <div
+        v-for="category in categories"
+        :key="category"
+        class="card col-6 col-xs-6 col-sm-6 col-md-6"
+      >
+        <nuxt-link
+          class="nlink"
+          :to="{
+            name: 'category-categoryId',
+            params: { categoryId: category.id },
+          }"
+        >
+          <div class="filter">
+            <img class="card-img" :src="category.IconUrl" alt="" />
+          </div>
+          <div class="card-img-overlay">
+            <p class="text-white">{{ category.CategoryName }}</p>
+          </div>
+        </nuxt-link>
       </div>
     </div>
   </div>
@@ -21,3 +35,33 @@ export default {
   },
 }
 </script>
+
+<style>
+.nlink {
+  width: 100%;
+  height: 100%;
+}
+
+.card {
+  padding: 0;
+}
+.filter {
+  background-color: black;
+  width: 100%;
+  height: 100%;
+}
+
+.card-img {
+  object-fit: cover;
+  width: 100%;
+  height: 100%;
+  opacity: 0.5;
+}
+.card-img-overlay {
+  padding: 0;
+  top: calc(50% - 0.5rem);
+  text-align: center;
+  font-weight: bold;
+  font-size: 30px;
+}
+</style>
