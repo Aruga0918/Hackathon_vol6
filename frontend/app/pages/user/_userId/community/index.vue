2cbaeeb6eeb0db305fb211c96e7cb447a3321474
マタタビのねこ <matatabinoneko0721@gmail.com>
2021-07-24 13:47:42 +0900

    update

---
+++ frontend/app/pages/user/_userId/community/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Comunities class="d-inline ml-auto mr-auto" :belongings="belongings" />
+    <Commodal />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      belongings: [],
+    }
+  },
+  mounted() {
+    this.belongings = this.$axios
+      .get(`/mock/users/${this.$route.params.userId}/community`)
+      .then((res) => {
+        this.belongings = res.data
+      })
+  },
+}
+</script>
