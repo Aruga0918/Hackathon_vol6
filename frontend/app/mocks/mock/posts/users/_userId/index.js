78d0fe9972fb9262f4b0110728970383f8c77581
マタタビのねこ <matatabinoneko0721@gmail.com>
2021-07-24 13:16:31 +0900

    投稿詳細ページを実装

--- frontend/app/mocks/mock/posts/users/_userId/index.js
+++ frontend/app/mocks/mock/posts/users/_userId/index.js
@@ -9,12 +9,28 @@ const posts = [
     shop_name: 'shop name 1',
     shop_icon_url:
       'https://www.google.com/imgres?imgurl=https%3A%2F%2Feverydayicons.jp%2Fwp%2Fwp-content%2Fthemes%2Feverydayicons%2Ficons%2Fthumbs%2Fei-shop.png&imgrefurl=https%3A%2F%2Feverydayicons.jp%2Ficons%2Fshop%2F&tbnid=YIodxarh64k9JM&vet=12ahUKEwios4nenu_xAhVKVJQKHWS4CVUQMygAegUIARC3AQ..i&docid=3FNo265Cl4KMSM&w=400&h=400&q=shop%20icon%20free&ved=2ahUKEwios4nenu_xAhVKVJQKHWS4CVUQMygAegUIARC3AQ',
-    message: '投稿のコメント',
+    message:
+      '投稿のコメント 投稿のコメント 投稿のコメント 投稿のコメント 投稿のコメント 投稿のコメント 投稿のコメント 投稿のコメント 投稿のコメント 投稿のコメント 投稿のコメント 投稿のコメント 投稿のコメント ',
     menu: [
       {
         menu_id: 1,
         name: 'メニューの名前1',
         price: 100,
+      },
+      {
+        menu_id: 2,
+        name: 'メニューの名前2',
+        price: 200,
+      },
+      {
+        menu_id: 2,
+        name: 'メニューの名前2',
+        price: 200,
+      },
+      {
+        menu_id: 2,
+        name: 'メニューの名前2',
+        price: 200,
       },
       {
         menu_id: 2,
